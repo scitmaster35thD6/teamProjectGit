8e4b971a34679c0536c851640bdeb37cb120f1aa
viper <viper@VIPER-DESKTOP>
2018-08-14 14:47:23 +0900

    Base File

---
+++ SEWorld/src/main/java/global/sesoc/seworld/exceptionhandler/ExecptionHandler.java
@@ -0,0 +1,15 @@
+package global.sesoc.seworld.exceptionhandler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExecptionHandler {
+
+	@ExceptionHandler(Exception.class)
+	public String errorHandler(Exception e) {
+		System.out.println(e);
+		e.printStackTrace();
+		return "/error";
+	}
+}
