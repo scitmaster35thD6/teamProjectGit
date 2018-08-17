0ca924f1d170866e5314b334dbe3ee5b6c7142c0
user <user@10.10.15.165>
2018-08-17 15:37:25 +0900

    웹에서 하이퍼링크 클릭 -> API에서 해외전시정보 가져와서 DB에 저장

--- SEWorld/src/main/java/global/sesoc/seworld/rest/CallRestWS.java
+++ SEWorld/src/main/java/global/sesoc/seworld/rest/CallRestWS.java
@@ -10,22 +10,19 @@ public class CallRestWS {
 		String addr = "http://www.gep.or.kr/rest/overseasExhibition?serviceKey=";
 		String serviceKey = "FsLyT%2F7neLBzi2BSryIlrXTpjg68D44fUqCcl2PO%2B3R%2B8%2FCnikXdveIici4eMl7YiBOm5Is1JnEWJteh9ux0BA%3D%3D";
 		String parameter = "";
-//		serviceKey = URLEncoder.encode(serviceKey, "UTF-8"); // 인증키(서비스키) url인코딩
 		parameter = parameter + "&" + "from=20180801";
 		parameter = parameter + "&" + "to=20180831";
 		parameter = parameter + "&" + "numOfRows=10";
 		parameter = parameter + "&" + "pageNo=1";
 		parameter = parameter + "&" + "type=json";
 		addr = addr + serviceKey + parameter;
-		System.out.println(addr);
-		
+//		System.out.println(addr);
 		URL url = new URL(addr);
 		InputStream in = url.openStream();
 		CachedOutputStream bos = new CachedOutputStream();
 		IOUtils.copy(in, bos);
 		bos.close();
 		in.close();
-		String result = bos.getOut().toString();
-		return result;
+		return bos.getOut().toString();
 	}
 }
--- SEWorld/src/main/java/global/sesoc/seworld/rest/Openapi.java
+++ SEWorld/src/main/java/global/sesoc/seworld/rest/Openapi.java
@@ -1,41 +1,14 @@
 package global.sesoc.seworld.rest;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
 public class Openapi {
 	public static void main(String[] args) throws Exception {
-		CallRestWS callRestWS = new CallRestWS();
-		String result = callRestWS.restClient();
-		System.out.println(result);
-		result.replace("&#034;", "\"\"");
-		System.out.println(result);
-//		JSONParser jsonParser = new JSONParser();
-//		JSONObject jsonObject = (JSONObject) jsonParser.parse(result);
-//		JSONArray jsonArray = (JSONArray) jsonObject.get("overseasExhibitionListArray");
-//		System.out.println(jsonArray.toArray());
-//		
-//		try {
-//			JSONParser jsonParser = new JSONParser();
-//			JSONObject jsonObj = (JSONObject) jsonParser.parse(result);
-//			JSONArray oela = (JSONArray) jsonObj.get("overseasExhibitionListArray");
-//
-//			System.out.println("=====Members=====");
-//			for (int i = 0; i < oela.size(); i++) {
-//				JSONObject tempObj = (JSONObject) oela.get(i);
-//				System.out.println("" + (i + 1) + "번째 exhibitionTitleKor" + tempObj.get("exhibitionTitleKor"));
-//				System.out.println("----------------------------");
-//			}
-//		} catch (ParseException e) {
-//			e.printStackTrace();
-//		}
-//		
-//		System.out.println("??");
+//		CallRestWS callRestWS = new CallRestWS();
+//		String result = callRestWS.restClient();
+//		String result2 = Jsoup.parse(callRestWS.restClient()).text();
+//		System.out.println(result);
+//		System.out.println(result2);
+		
+		XmlParsingTest xmlParsingTest = new XmlParsingTest();
+		xmlParsingTest.parse();
 	}
 }
