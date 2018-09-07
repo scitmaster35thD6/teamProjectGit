b79632d41f2f9606489f8da702c16da395333cc2
Viper <youngbina@hotmail.com>
2018-09-07 22:52:02 +0900

    2018-09-07 23시 SQL문 수정
    제약조건 없이 DROP TABLE 가능

--- SEWorld/src/main/resources/180905_c6_v4_drop_table.sql
+++ SEWorld/src/main/resources/180905_c6_v4_drop_table.sql
@@ -12,25 +12,25 @@ DROP SEQUENCE c6_exhibition_seq;
 DROP SEQUENCE c6_cities_seq;
 
 
-DROP TABLE c6_board_reply;
-DROP TABLE c6_board_file;
-DROP TABLE c6_board;
+DROP TABLE c6_board_reply cascade constraints purge;
+DROP TABLE c6_board_file cascade constraints purge;
+DROP TABLE c6_board cascade constraints purge;
 -- DROP TABLE c6_ask_reply;
 -- DROP TABLE c6_ask_file;
 -- DROP TABLE c6_ask;
-DROP TABLE c6_comment_reply_likes;
-DROP TABLE c6_comment_reply;
-DROP TABLE c6_comment_likes;
-DROP TABLE c6_comment;
+DROP TABLE c6_comment_reply_likes cascade constraints purge;
+DROP TABLE c6_comment_reply cascade constraints purge;
+DROP TABLE c6_comment_likes cascade constraints purge;
+DROP TABLE c6_comment cascade constraints purge;
 -- DROP TABLE c6_participating;
-DROP TABLE c6_wishing;
-DROP TABLE c6_member_profile;
-DROP TABLE c6_member;
-DROP TABLE c6_exhibition;
-DROP TABLE c6_cities;
-DROP TABLE c6_countries;
-DROP TABLE c6_regions;
-DROP TABLE c6_exbtinfo;
+DROP TABLE c6_wishing cascade constraints purge;
+DROP TABLE c6_member_profile cascade constraints purge;
+DROP TABLE c6_member cascade constraints purge;
+DROP TABLE c6_exhibition cascade constraints purge;
+DROP TABLE c6_cities cascade constraints purge;
+DROP TABLE c6_countries cascade constraints purge;
+DROP TABLE c6_regions cascade constraints purge;
+DROP TABLE c6_exbtinfo cascade constraints purge;
 
 
 PURGE RECYCLEBIN;
