a934fe3cc3fffd796ba32795915060dd22553966
furaha <lhmgreen@daum.net>
2018-10-04 13:44:09 +0900

    캘린
    캘린

--- SEWorld/src/main/webapp/resources/dist/js/pages/calendar/cal-init.js
+++ SEWorld/src/main/webapp/resources/dist/js/pages/calendar/cal-init.js
@@ -57,7 +57,7 @@
 function toTimeObject(str){
 	//alert("str"+str);
 	var year = str.substring(0, 4);
-	var month = str.substring(4, 6);
+	var month = str.substring(4, 6)-1;
 	var day  = str.substring(6, 8);
 	/*alert(year+"이얼");
 	alert(month+"먼스");
